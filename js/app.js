d73736031b7475441ac263fd382bb16a10246bda
Laura <lauradhamilton@gmail.com>
2014-07-26 13:32:04 -0500

    Adding a new question form

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ triviaApp.config(['$routeProvider', function($routeProvider) {
       templateUrl: 'partials/question-list.html',
       controller: 'QuestionListCtrl'
     }).
+    when('/questions/new', {
+      templateUrl: 'partials/question-form.html',
+      controller: 'QuestionNewCtrl'
+    }).
     otherwise({
       redirectTo: '/questions'
     });
@@ -14,4 +18,16 @@ triviaApp.config(['$routeProvider', function($routeProvider) {
 triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope, $firebase) {
   var firebaseUrl = "https://triviaapp.firebaseio.com/questions";
   $scope.questions = $firebase(new Firebase(firebaseUrl));
+}]);
+
+triviaApp.controller('QuestionNewCtrl', ['$scope', '$firebase', '$location', function($scope, $firebase, $location) {
+  $scope.question = {}
+
+  $scope.persistQuestion = function(question) {
+    var firebaseUrl = "https://triviaapp.firebaseio.com/questions";
+    $scope.questions = $firebase(new Firebase(firebaseUrl));
+    $scope.questions.$add(question).then(function(ref) {
+      $location.url('questions');
+    });
+  };
 }]);
