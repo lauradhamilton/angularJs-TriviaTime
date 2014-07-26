d6524763f52044b467dd313cdc5628cf084faf3a
Laura <lauradhamilton@gmail.com>
2014-07-26 13:05:49 -0500

    Addangular routing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-var triviaApp = angular.module('triviaApp', ['firebase']);
+var triviaApp = angular.module('triviaApp', ['firebase','ngRoute']);
+
+triviaApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/questions', {
+      templateUrl: 'partials/question-list.html',
+      controller: 'QuestionListCtrl'
+    }).
+    otherwise({
+      redirectTo: '/questions'
+    });
+}]);
 
 triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope, $firebase) {
   var firebaseUrl = "https://triviaapp.firebaseio.com/questions";
